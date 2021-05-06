38ab956c3ed8eabb9eb94c7ba78b0f798501d7ab
mvilella <mvilella@obsa.ad>
2021-05-06 12:56:02 +0200

    Add main navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
 	<section class="fill" :data-tag="t('navigation.menu')">
+		<div class="nav-grid">
+			<router-link
+				v-for="route in routes"
+				:key="route.name"
+				:class="`nav-${route.name}`"
+				:to="route.path">
+				<h1 :class="route.title">{{ t(`navigation.${route.name}`) }}</h1>
+				<figure>
+					<img :src="route.image.url">
+					<figcaption v-if="route.image.attribution">
+						{{ route.image.attribution }}
+					</figcaption>
+				</figure>
+			</router-link>
+		</div>
 	</section>
 </template>
 
 <script>
 import { useI18n } from 'vue-i18n';
+import { routes } from '/@/config/global.yaml';
 
 export default {
 	name: 'Home',
 	setup() {
 		const { t } = useI18n();
-		return { t };
+		return { t, routes };
 	},
 };
 </script>
