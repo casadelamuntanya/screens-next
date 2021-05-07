48bc934d786df9fc0c2a7f09abb75b01922496d7
mvilella <mvilella@obsa.ad>
2021-05-07 22:04:17 +0200

    Add Equipment safety view
    Too many whitespace. Fullfill with content or give extra margins somewhere

---
+++ src/views/safety/SafetyEquipment.vue
@@ -0,0 +1,42 @@
+<template>
+	<figure class="ungutter">
+		<img src="/images/trekking_gear.png">
+	</figure>
+	<section>
+		<h1>{{ t('safety.equipment.equip_yourself') }}</h1>
+		<p>{{ t('safety.equipment.proper_equipment') }}</p>
+	</section>
+	<section class="columns">
+		<section class="column column--2" :data-tag-pre="t('safety.equipment.essentials')">
+			<ul class="icon-list list-cols-2">
+				<li v-for="item in equipment.essential" :key="item">
+					<img v-svg-inline :src="`/images/vectors/equipment/${item}.svg`" class="icon">
+					{{ t(`safety.equipment.items.${item}`) }}
+				</li>
+			</ul>
+		</section>
+		<section class="column" :data-tag-pre="t('safety.equipment.recommended')">
+			<ul class="icon-list">
+				<li v-for="item in equipment.recommended" :key="item">
+					<img v-svg-inline :src="`/images/vectors/equipment/${item}.svg`" class="icon">
+					{{ t(`safety.equipment.items.${item}`) }}
+				</li>
+			</ul>
+		</section>
+	</section>
+	<section class="note">{{ t('safety.equipment.disclaimer') }}</section>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n';
+import { equipment } from '/@/config/views/safety.yaml';
+
+export default {
+	name: 'SafetyEquipment',
+	setup() {
+		const { t } = useI18n();
+
+		return { t, equipment };
+	},
+};
+</script>
--- src/views/safety/routes.js
+++ src/views/safety/routes.js
@@ -1,7 +1,9 @@
 import SafetyGeneral from './SafetyGeneral.vue';
 import SafetyRescue from './SafetyRescue.vue';
+import SafetyEquipment from './SafetyEquipment.vue';
 
 export default [
 	{ path: '', name: 'general', component: SafetyGeneral },
 	{ path: 'rescue', name: 'rescue', component: SafetyRescue },
+	{ path: 'equipment', name: 'equipment', component: SafetyEquipment },
 ];
