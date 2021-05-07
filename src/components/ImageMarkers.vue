05c20257628d05fd96390ea16ed528e82e33b669
mvilella <mvilella@obsa.ad>
2021-05-07 11:06:44 +0200

    Add ImageMarkers and VideoEmbed components

---
+++ src/components/ImageMarkers.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="image-markers">
+		<slot />
+		<div
+			v-for="(marker, i) in markers"
+			:key="`${marker.top}.${marker.left}`"
+			:style="`top:${marker.top}%;left:${marker.left}%;`"
+			class="image-markers__marker">
+			<slot name="marker" :marker="marker" :i="i" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ImageMarkers',
+	props: { markers: { type: Array, required: true } },
+};
+</script>
+
+<style lang="scss" scoped>
+.image-markers {
+	display: inline-block;
+	position: relative;
+	overflow: visible;
+
+	&__marker {
+		display: block;
+		position: absolute;
+		transform: translate(-50%, -50%);
+		z-index: 0;
+		overflow: visible;
+
+		&:hover { z-index: 1; }
+	}
+}
+</style>
