5a17866ca42bbc1753c15a727f6d03d09e9e4633
mvilella <mvilella@obsa.ad>
2021-05-10 10:32:55 +0200

    Add Wildlife safety view

--- src/views/safety/routes.js
+++ src/views/safety/routes.js
@@ -1,9 +1,11 @@
 import SafetyGeneral from './SafetyGeneral.vue';
 import SafetyRescue from './SafetyRescue.vue';
 import SafetyEquipment from './SafetyEquipment.vue';
+import SafetyWildlife from './SafetyWildlife.vue';
 
 export default [
 	{ path: '', name: 'general', component: SafetyGeneral },
 	{ path: 'rescue', name: 'rescue', component: SafetyRescue },
 	{ path: 'equipment', name: 'equipment', component: SafetyEquipment },
+	{ path: 'wildlife', name: 'wildlife', component: SafetyWildlife },
 ];
