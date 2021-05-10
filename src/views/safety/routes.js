88ec253b29d62ee3e97fe67232b76829dc03fa5c
mvilella <mvilella@obsa.ad>
2021-05-10 15:40:14 +0200

    Add Thunderstorm safety view

--- src/views/safety/routes.js
+++ src/views/safety/routes.js
@@ -2,10 +2,12 @@ import SafetyGeneral from './SafetyGeneral.vue';
 import SafetyRescue from './SafetyRescue.vue';
 import SafetyEquipment from './SafetyEquipment.vue';
 import SafetyWildlife from './SafetyWildlife.vue';
+import SafetyThunderstorm from './SafetyThunderstorm.vue';
 
 export default [
 	{ path: '', name: 'general', component: SafetyGeneral },
 	{ path: 'rescue', name: 'rescue', component: SafetyRescue },
 	{ path: 'equipment', name: 'equipment', component: SafetyEquipment },
 	{ path: 'wildlife', name: 'wildlife', component: SafetyWildlife },
+	{ path: 'thunderstorm', name: 'thunderstorm', component: SafetyThunderstorm },
 ];
