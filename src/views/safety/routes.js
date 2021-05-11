437ec7c13762a2f27305f0b694155c0213919646
mvilella <mvilella@obsa.ad>
2021-05-11 17:45:26 +0200

    Add Hypothermia safety view

--- src/views/safety/routes.js
+++ src/views/safety/routes.js
@@ -3,6 +3,7 @@ import SafetyRescue from './SafetyRescue.vue';
 import SafetyEquipment from './SafetyEquipment.vue';
 import SafetyWildlife from './SafetyWildlife.vue';
 import SafetyThunderstorm from './SafetyThunderstorm.vue';
+import SafetyHypothermia from './SafetyHypothermia.vue';
 
 export default [
 	{ path: '', name: 'general', component: SafetyGeneral },
@@ -10,4 +11,5 @@ export default [
 	{ path: 'equipment', name: 'equipment', component: SafetyEquipment },
 	{ path: 'wildlife', name: 'wildlife', component: SafetyWildlife },
 	{ path: 'thunderstorm', name: 'thunderstorm', component: SafetyThunderstorm },
+	{ path: 'hypothermia', name: 'hypothermia', component: SafetyHypothermia },
 ];
