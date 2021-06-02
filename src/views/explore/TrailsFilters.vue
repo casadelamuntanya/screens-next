286e328db1db660c5cd2f3df6d7b20be063f540b
mvilella <mvilella@obsa.ad>
2021-06-02 15:33:17 +0300

    Add featured profiles filter

--- src/views/explore/TrailsFilters.vue
+++ src/views/explore/TrailsFilters.vue
@@ -7,14 +7,26 @@
 			</label>
 		</li>
 	</ul>
+	<ul v-if="filter === 'featured'" class="profiles">
+		<li v-for="name in profiles" :key="name">
+			<label>
+				<input v-model="profile" :value="name" type="radio">
+				<inline-svg :src="`/images/vectors/profiles/${name}.svg`" />
+				<span>{{ t(`explore.profiles.${name}`, name) }}</span>
+			</label>
+		</li>
+	</ul>
 </template>
 
 <script>
 import { ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
+import InlineSvg from 'vue-inline-svg';
+import config from '/@/config/views/explore.yaml';
 
 export default {
 	name: 'TrailsFilters',
+	components: { InlineSvg },
 	props: { modelValue: { type: Function, default: () => true } },
 	emits: ['update:modelValue'],
 	setup(props, { emit }) {
@@ -22,17 +34,21 @@ export default {
 
 		const filter = ref('all');
 		const currentMonth = new Date().toLocaleString('en', { month: 'short' }).toUpperCase();
+		const { profiles } = config.filters;
+		const profile = ref(undefined);
 
 		const filters = {
 			all: () => true,
 			trending: trail => trail.do_it_now?.includes(currentMonth),
+			featured: trail => !profile.value || trail.profile?.includes(profile.value),
 		};
 
 		watch(filter, name => {
+			profile.value = undefined;
 			emit('update:modelValue', filters[name]);
 		}, { immediate: true });
 
-		return { t, filters, filter };
+		return { t, filters, filter, profiles, profile };
 	},
 };
 </script>
