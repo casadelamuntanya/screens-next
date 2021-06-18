2d5ba9b30397b2056694c3f01fc371270ef94d4a
mvilella <mvilella@obsa.ad>
2021-06-18 14:51:08 +0200

    Add extended guide profile

--- src/views/Guides.vue
+++ src/views/Guides.vue
@@ -15,10 +15,27 @@
 	<section class="stack" :data-tag="t('guides.mountain_guides')">
 		<ul v-if="guides.length" class="guides">
 			<li v-for="guide in guides" :key="guide.name" :class="guideStatus(guide)">
-				<figure class="cover">
-					<img :src="guide.photo[0].url" class="faded">
-				</figure>
-				<h2>{{ guide.name }}<small>{{ guide.surname }}</small></h2>
+				<div class="guide__preview" tabindex="1">
+					<figure class="cover">
+						<img :src="guide.photo[0].url" class="faded">
+					</figure>
+					<h2>{{ guide.name }}<small>{{ guide.surname }}</small></h2>
+				</div>
+				<!-- extended profile -->
+				<div class="guide__extended">
+					<img v-if="guide.qr" :src="guide.qr[0].url" class="qr-code">
+					<figure class="cover">
+						<img :src="guide.photo[guide.photo.length > 1 ? 1 : 0].url" class="faded">
+					</figure>
+					<h2>{{ guide.name }}<small>{{ guide.surname }}</small></h2>
+					<p>{{ guide[`bio_${locale}`] }}</p>
+					<p><strong>{{ t('guides.share_knowledge') }}</strong></p>
+					<ul class="guide__knowledge">
+						<li v-for="topic in guide.knowledge" :key="topic">
+							{{ t(`guides.knowledge.${topic}`) }}
+						</li>
+					</ul>
+				</div>
 			</li>
 		</ul>
 		<p v-else class="loading">{{ t('guides.loading') }}</p>
@@ -38,7 +55,7 @@ export default {
 	name: 'Guides',
 	components: { Selector },
 	setup() {
-		const { t } = useI18n();
+		const { t, locale } = useI18n();
 		const guides = ref([]);
 		const filters = reactive({
 			languages: [],
@@ -61,7 +78,7 @@ export default {
 			[filters.language] = filters.languages;
 		});
 
-		return { t, guides, guideStatus, filters };
+		return { t, locale, guides, guideStatus, filters };
 	},
 };
 </script>
@@ -75,7 +92,6 @@ export default {
 	.guide {
 		flex: 0 0 23%;
 		margin: auto 5%;
-		transition: all 0.5s ease;
 
 		h2 {
 			font-size: var(--xl);
@@ -94,10 +110,55 @@ export default {
 			padding-top: 120%;
 		}
 
-		&.inactive {
-			filter: saturate(0) opacity(0.1);
-			pointer-events: none;
+	}
+
+	&:focus-within .guide__preview,
+	.inactive .guide__preview  {
+		filter: saturate(0) opacity(0.1);
+		pointer-events: none;
+	}
+
+	.guide__preview {
+		transition: all 0.5s ease;
+	}
+
+	.guide__extended {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		margin: 0;
+		opacity: 0;
+		width: 50%;
+		visibility: hidden;
+		z-index: 2;
+		transform: translate(-50%, -50%);
+		transition: all 0.5s ease;
+
+		.qr-code {
+			position: absolute;
+			top: 0;
+			right: 0;
+			margin: var(--s);
+			height: 5rem;
+			width: 4rem;
 		}
+	}
+
+	.guide__preview:focus + .guide__extended {
+		visibility: visible;
+		opacity: 1;
+	}
+
+	.guide__knowledge li {
+		display: inline-block;
+		border: 1px solid var(--color-primary);
+		color: var(--color-primary);
+		padding: var(--xs) var(--s);
+		margin: 1px;
+		border-radius: var(--border-radius);
+		opacity: 0.75;
+		font-size: var(--s);
+		text-transform: uppercase;
 	}
 }
 </style>
