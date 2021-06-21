f6f9c83ac45624bab364f4d4e3e2759f26cb5eac
mvilella <mvilella@obsa.ad>
2021-06-21 16:50:14 +0200

    Add router guards
    Router guards trigger in and out animations when route changes
    
    [+] Include generic fade animations

--- src/router.js
+++ src/router.js
@@ -1,3 +1,4 @@
+import { nextTick } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '/@/views/Home.vue';
 import About from '/@/views/About.vue';
@@ -20,6 +21,30 @@ const router = createRouter({
 		{ path: '/play', name: 'play', component: Play, children: PlayRoutes },
 		{ path: '/:pathMatch(.*)', component: NotFound },
 	],
+});
+
+// Trigger enter animates
+router.afterEach(async () => {
+	await nextTick();
+	const elements = Array.from(document.querySelectorAll('[data-animate]')) || [];
+	elements.forEach(el => {
+		const { animate = 'fade', animateDelayIn = '0s' } = el.dataset;
+		el.style.setProperty('--animate-animation', `${animate}-in`);
+		el.style.setProperty('--animate-delay', animateDelayIn);
+		el.classList.add('animate-in');
+	});
+});
+
+// Trigger leave animates
+router.beforeEach(async () => {
+	const elements = Array.from(document.querySelectorAll('[data-animate]')) || [];
+	await Promise.all(elements.map(el => new Promise(resolve => {
+		const { animate = 'fade', animateDelayOut = '0s' } = el.dataset;
+		el.style.setProperty('--animate-animation', `${animate}-out`);
+		el.style.setProperty('--animate-delay', animateDelayOut);
+		el.classList.add('animate-out');
+		el.addEventListener('animationend', () => resolve());
+	})));
 });
 
 export default router;
