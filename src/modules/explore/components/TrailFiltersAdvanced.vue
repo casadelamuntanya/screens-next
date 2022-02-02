4ba7dfd10db9038369c841662abcf67a28c592e8
mvilella <mvilella@obsa.ad>
2022-02-02 11:02:03 +0100

    feat: Add button to clear trail advances filters

--- src/modules/explore/components/TrailFiltersAdvanced.vue
+++ src/modules/explore/components/TrailFiltersAdvanced.vue
@@ -1,21 +1,26 @@
 <template>
-  <section class="columns advanced-filters">
-    <div class="column">
-      <fieldset v-for="(options, name) in filters.ranges" :key="name" class="range">
-        <legend>{{ t(`explore.filters.${name}`) }}</legend>
-        <em>{{ rangeLabel(name) }}</em>
-        <slider v-model="ranges[name]" v-bind="options" :tooltips="false" />
-      </fieldset>
+  <section class="advanced-filters">
+    <div class="columns">
+      <div class="column">
+        <fieldset v-for="(options, name) in RANGES" :key="name" class="range">
+          <legend>{{ t(`explore.filters.${name}`) }}</legend>
+          <em>{{ rangeLabel(name) }}</em>
+          <slider v-model="ranges[name]" v-bind="options" :tooltips="false" />
+        </fieldset>
+      </div>
+      <div class="column">
+        <fieldset v-for="(options, name) in PICKS" :key="name" class="picker">
+          <legend>{{ t(`explore.filters.${name}`) }}</legend>
+          <label v-for="item in options" :key="item">
+            <input v-model="picks[name]" :value="item" type="checkbox">
+            {{ t(`explore.${name}.${item}`, item) }}
+          </label>
+        </fieldset>
+      </div>
     </div>
-    <div class="column">
-      <fieldset v-for="(options, name) in filters.picks" :key="name" class="picker">
-        <legend>{{ t(`explore.filters.${name}`) }}</legend>
-        <label v-for="item in options" :key="item">
-          <input v-model="picks[name]" :value="item" type="checkbox">
-          {{ t(`explore.${name}.${item}`, item) }}
-        </label>
-      </fieldset>
-    </div>
+    <p class="buttons">
+      <button class="btn" @click="reset">{{ t('explore.clear_filters') }}</button>
+    </p>
   </section>
 </template>
 
@@ -24,7 +29,18 @@ import { reactive, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import Slider from '@vueform/slider';
 import * as converter from 'units-converter';
-import { filters } from '../config.yaml';
+import config from '../config.yaml';
+
+const { ranges: RANGES, picks: PICKS } = config.filters;
+
+const DEFAULT = {
+  picks: { wildlife: [], culture: [], origin: [] },
+  ranges: {
+    distance: [RANGES.distance.min, RANGES.distance.max],
+    duration: [RANGES.duration.min, RANGES.duration.max],
+    drop_positive: [RANGES.drop_positive.min, RANGES.drop_positive.max],
+  },
+};
 
 /* eslint-disable camelcase */
 export default {
@@ -34,27 +50,27 @@ export default {
   emits: ['update:modelValue'],
   setup(props, { emit }) {
     const { t } = useI18n();
-    const { distance, duration, drop_positive } = filters.ranges;
-    const picks = reactive({ wildlife: [], culture: [], origin: [] });
-    const ranges = reactive({
-      distance: [distance.min, distance.max],
-      duration: [duration.min, duration.max],
-      drop_positive: [drop_positive.min, drop_positive.max],
-    });
+    const picks = reactive({ ...DEFAULT.picks });
+    const ranges = reactive({ ...DEFAULT.ranges });
 
     const rangeLabel = name => {
-      const { type, from, to } = filters.ranges[name].unit;
+      const { type, from, to } = RANGES[name].unit;
       const min = converter[type](ranges[name][0]).from(from).to(to).value;
       const max = converter[type](ranges[name][1]).from(from).to(to).value;
-      return ranges[name][1] === filters.ranges[name].max
+      return ranges[name][1] === RANGES[name].max
         ? t('explore.filters.range.more_than', { min, unit: to })
         : t('explore.filters.range.from_to', { min, max, unit: to });
+    };
+
+    const reset = () => {
+      Object.assign(picks, DEFAULT.picks);
+      Object.assign(ranges, DEFAULT.ranges);
     };
 
     watch([picks, ranges], () => {
       const filter = trail => {
         const inRanges = Object.entries(ranges).every(([attribute, range]) => {
-          const max = range[1] === filters.ranges[attribute].max ? Infinity : range[1];
+          const max = range[1] === RANGES[attribute].max ? Infinity : range[1];
           return range[0] <= trail[attribute] && trail[attribute] <= max;
         });
         const hasPicks = Object.entries(picks).every(([attribute, pick]) => {
@@ -66,7 +82,7 @@ export default {
       emit('update:modelValue', filter);
     }, { immediate: true });
 
-    return { t, picks, ranges, rangeLabel, filters };
+    return { t, RANGES, PICKS, ranges, picks, rangeLabel, reset };
   },
 };
 </script>
